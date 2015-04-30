483e2dabd566bbbefd7e08e4043b5e6059dec34e
Karl Ingi <karlingie@gmail.com>
2015-04-30 15:41:25 +0200

    Xbee communication V0.1 - Tx and Rx data packages

--- wireless_cummonication.h
+++ wireless_cummonication.h
@@ -2,14 +2,13 @@
 #define wireless_communication_h
 #include "global.h"
 #include "XBee.h"
-//#include <stdlib.h>
+
+#define COORDINATOR_ADDR	0x0013a200,0x40B5BBD0
+#define BOAT_1_ADDR			0x0013A200,0x40B5BBF0
+#define BOAT_2_ADDR			0x0013A200,0x40B5BBF5
+#define BOAT_3_ADDR			0x0013A200,0x40B48973
+
 class wireless_communication_class
-
-#define COORDINATOR 0x0013a200,0x40B5BBD0
-#define BOAT_1		0x0013A200,0x40B5BBF0
-#define BOAT_2		0x0013A200,0x40B5BBF5
-#define BOAT_3		0x0013A200,0x40B48973
-
 {
 public:
 	wireless_communication_class(){}
@@ -18,25 +17,39 @@ public:
 		xbee.setSerial(Serial2);
 		xbee.begin(Serial2);
 	}
+	enum XBEE_ID {
+		boat1,
+		boat2,
+		boat3,
+		boat4,
+		coordinator
+	};
+	//Data package to send and receive
 	typedef struct{          
-		double lattitude;
-		double longtitude;
+		XBEE_ID ID;
+		double latitude;
+		double longitude;
 		float  bearing;
 		float  speed;
 	}DATA_;
 
-	void send_info(double lattitude, double longtitude, float bearing, float speed){
-		outData.lattitude = lattitude;
-		outData.longtitude = longtitude;
+	void send_info(double latitude, double longitude, float bearing, float speed){
+		outData.ID = boat1;											//*********** Set ID of sender ( coordinator = 4	boat1 = 0	boat2 = 1 ... )
+		outData.latitude = latitude;
+		outData.longitude = longitude;
 		outData.bearing = bearing;
 		outData.speed = speed;
 
 		uint8_t pay_load[sizeof(outData)];
-		memcpy(pay_load, &outData, sizeof(outData));			//DATA_ to uint8
+		memcpy(pay_load, &outData, sizeof(DATA_));					//Convert DATA_ to uint8
 
-		addr64 = XBeeAddress64(COORDINATOR);							//		BOAT_1		COORDINATOR
-		zbTx = ZBTxRequest(addr64, pay_load, sizeof(pay_load));
+		addr64 = XBeeAddress64(COORDINATOR_ADDR);					//*********** Set Address of receiver ( BOAT_1_ADDR	BOAT_2_ADDR	COORDINATOR_ADDR	...)
+		zbTx = ZBTxRequest(addr64, pay_load, sizeof(DATA_));
 		xbee.send(zbTx);
+
+		//addr64 = XBeeAddress64(BOAT_1_ADDR);						//*********** Set Address of receiver ( BOAT_1_ADDR	BOAT_2_ADDR	COORDINATOR_ADDR	...)
+		//zbTx = ZBTxRequest(addr64, pay_load, sizeof(DATA_));
+		//xbee.send(zbTx);
 	}
 	void get_info(void){
 		xbee.readPacket();
@@ -46,16 +59,19 @@ public:
 				// got a zb rx packet
 				xbee.getResponse().getZBRxResponse(rx);// now fill our zb rx class
 			}
-			inData = {};
-			uint8_t b[sizeof(DATA_)];
-			memcpy(&inData, rx.getData(), sizeof(DATA_));		//uint8 to DATA_
+			inData = {};//clear inData package
+			memcpy(&inData, rx.getData(), sizeof(DATA_));				//Convert uint8 to DATA_
 
-			Serial.println(inData.lattitude,7);
-			Serial.println(inData.longtitude, 7);
+			update_other_boats_location(inData);
+
+			Serial.println(inData.ID);
+			Serial.println(inData.latitude,7);
+			Serial.println(inData.longitude, 7);
 			Serial.println(inData.bearing, 7);
 			Serial.println(inData.speed, 7);
 			Serial.print('\r');
 			Serial.print('\n');
+
 		}
 		else if(global.debug_handler.wireless_communication_debug) Serial.println("didnt get anything");
 	}
@@ -75,12 +91,12 @@ private:
 	ZBRxResponse rx = ZBRxResponse();					// create reusable response objects for responses we expect to handle 
 	//ModemStatusResponse msr = ModemStatusResponse();
 
-	void update_other_boats_location(int i /*change*/){
-		global.other_boats[i].bearing = 1;
-		global.other_boats[i].latitude = 1;
-		global.other_boats[i].longtitude = 1;
-		global.other_boats[i].speed = 1;
-		global.other_boats[i].is_valid_boat = true;
+	void update_other_boats_location(DATA_ inData){
+		global.other_boats[inData.ID].bearing = inData.bearing;
+		global.other_boats[inData.ID].latitude = inData.latitude;
+		global.other_boats[inData.ID].longtitude = inData.longitude;
+		global.other_boats[inData.ID].speed = inData.speed;
+		global.other_boats[inData.ID].is_valid_boat = true;
 	}
 
 };
