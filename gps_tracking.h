08584ea0eba633c961c41a13e1554a45f39ad34f
kavjik <kavjik@gmail.com>
2015-05-22 10:43:10 +0200

    fix for previous

--- gps_tracking.h
+++ gps_tracking.h
@@ -48,12 +48,12 @@ void gps_tracking() {
 
 		//global.gps_data.location.latitude = (fmod(global.GPS_module.latitude, 100) / (60)) + floor(global.GPS_module.latitude / 100);
 		
-		global.gps_data.location.latitude = floor(global.GPS_module.latitude / 100) + (global.GPS_module.latitude / 100 - floor(global.GPS_module.latitude / 100)) * 100 / 60;
+		if (is_gps_module_latittude_valid()) global.gps_data.location.latitude = floor(global.GPS_module.latitude / 100) + (global.GPS_module.latitude / 100 - floor(global.GPS_module.latitude / 100)) * 100 / 60;
 		//global.gps_data.location.longtitude = (fmod(global.GPS_module.longitude, 100) / (60)) + floor(global.GPS_module.longitude / 100);
 
 		if (is_gps_module_longtitude_valid()) global.gps_data.location.longtitude = floor(global.GPS_module.longitude / 100) + (global.GPS_module.longitude / 100 - floor(global.GPS_module.longitude / 100)) * 100 / 60;
 		//NO NO NO I DONT WANT TO DO THIS, but the data i get for the longtitude is often invallid, i feel i dont have a choice.
-		if (is_gps_module_latittude_valid()) global.gps_data.location.bearing = global.GPS_module.angle;
+		global.gps_data.location.bearing = global.GPS_module.angle;
 		global.gps_data.location.speed = global.GPS_module.speed * 0.51444; //constant to convert from knots to m/s
 
 		
