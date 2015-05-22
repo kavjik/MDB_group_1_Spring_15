b3e649e4c6b3e71497cc888bf78b24e3768dcb1c
kavjik <kavjik@gmail.com>
2015-05-22 10:35:28 +0200

    introduced the same fix for lattitude, I DONT WANT TO DO THIS, BUT ITS THE ONLY WAY.

--- gps_tracking.h
+++ gps_tracking.h
@@ -8,14 +8,22 @@
 
 // test comment
 
-//it would make more sense to have this as a functino that is called every time we have a new gps coordinate, but when i tried that, nothing worked, so i do it this way instead. 
+
 bool is_gps_module_longtitude_valid(void){ //this is a very ugly fix to a very ugly problem
-	if (global.GPS_module.longitude > 10.5) return false;
-	if (global.GPS_module.longitude < 9.1) return false;
+	if (global.GPS_module.longitude < 10.5) return false;
+	if (global.GPS_module.longitude > 9.1) return false;
 
 	static int counter = 0;
 
 }
+bool is_gps_module_latittude_valid(void){ //this is a very ugly fix to a very ugly problem
+	if (global.GPS_module.latitude < 58) return false;
+	if (global.GPS_module.latitude > 54) return false;
+
+	static int counter = 0;
+
+}
+//it would make more sense to have this as a functino that is called every time we have a new gps coordinate, but when i tried that, nothing worked, so i do it this way instead. 
 void gps_tracking() {
 
 	global.gps_data.fix = false; //set it false just to be sure, if had some problems with it being true when not meant to.
@@ -43,9 +51,9 @@ void gps_tracking() {
 		global.gps_data.location.latitude = floor(global.GPS_module.latitude / 100) + (global.GPS_module.latitude / 100 - floor(global.GPS_module.latitude / 100)) * 100 / 60;
 		//global.gps_data.location.longtitude = (fmod(global.GPS_module.longitude, 100) / (60)) + floor(global.GPS_module.longitude / 100);
 
-		if (is_gps_module_longtitude_valid) global.gps_data.location.longtitude = floor(global.GPS_module.longitude / 100) + (global.GPS_module.longitude / 100 - floor(global.GPS_module.longitude / 100)) * 100 / 60;
+		if (is_gps_module_longtitude_valid()) global.gps_data.location.longtitude = floor(global.GPS_module.longitude / 100) + (global.GPS_module.longitude / 100 - floor(global.GPS_module.longitude / 100)) * 100 / 60;
 		//NO NO NO I DONT WANT TO DO THIS, but the data i get for the longtitude is often invallid, i feel i dont have a choice.
-		global.gps_data.location.bearing = global.GPS_module.angle;
+		if (is_gps_module_latittude_valid()) global.gps_data.location.bearing = global.GPS_module.angle;
 		global.gps_data.location.speed = global.GPS_module.speed * 0.51444; //constant to convert from knots to m/s
 
 		
